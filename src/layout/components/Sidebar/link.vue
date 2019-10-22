4dd7589c2091bc78ba61d7da1b96a3986f5134d8
heping <1316143408@qq.com>
2019-10-23 00:09:47 +0800

    link

--- src/layout/components/Sidebar/link.vue
+++ src/layout/components/Sidebar/link.vue
@@ -1,5 +1,34 @@
 <template>
-    <component>
-        
+    <component v-bind="linkProps(to)">
+        <slot></slot>
     </component>
 </template>
+
+<script>
+import {isExternal} from '@/utils/validate.js'
+export default {
+    props:{
+        to:{
+            type:String,
+            required:true
+        }
+    },
+    methods:{
+        linkProps(url){
+            if(isExternal(url)){
+                return {
+                    is:'a',
+                    herf:url,
+                    target:'_blank',
+                    rel:'noopener'
+                }
+            }else {
+                return {
+                    is:'router_link',
+                    to:url
+                }
+            }
+        }
+    }
+}
+</script>
