7b08ef585535bb96f13a98b34329dadae08c60eb
heping <1316143408@qq.com>
2019-10-28 01:15:30 +0800

    路由权限

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -2,3 +2,18 @@ import {
   asyncRoutes,
   constantRoutes
 } from '@/router'
+
+/**
+ * 根据meta.role确认该用户是否有权限
+ * @param roles
+ * @param route
+ * 
+ */
+
+function hasPermission(roles, route) {
+  if (route.meta && route.meta.role) {
+    return roles.some(role => route.meta.roles.includes(role))
+  } else {
+    return true
+  }
+}
